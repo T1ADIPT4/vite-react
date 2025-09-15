ef96cc5224c11f63700bcd06e20f53ca753d0bf4
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-15 18:51:36 +0700

    Update meechain.ts
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- vite-react/src/config/meechain.ts
+++ vite-react/src/config/meechain.ts
@@ -1,1 +1,12 @@
 
+{
+  "network": "mee-mainnet",
+  "chainId": 2025,
+  "rpcUrl": "https://rpc.meechain.io",
+  "explorerUrl": "https://explorer.meechain.io",
+  "contract": {
+    "name": "MeeToken",
+    "address": "0x1234567890abcdef1234567890abcdef12345678",
+    "abiPath": "/src/contracts/MeeToken.abi.json"
+  }
+}
