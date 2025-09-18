47e8a38cd017686f4df74c1b2e40a9f6dbcf6c0e
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-18 17:48:18 +0700

    Create networks.ts
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

---
+++ configs/configs/networks.ts
@@ -0,0 +1,8 @@
+
+// configs/networks.ts
+export const networks = {
+  ethereum: { rpc: "...", contract: "...", quests: [...] },
+  polygon: { rpc: "...", contract: "...", quests: [...] },
+  arbitrum: { rpc: "...", contract: "...", quests: [...] },
+};
+
