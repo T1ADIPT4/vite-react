68b8ad9e9a8c2a47563af830787107098610d6c0
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-18 18:08:38 +0700

    Delete configs/configs directory
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- configs/configs/networks.ts
+++
@@ -1,9 +0,0 @@
-import { networks } from './configs/networks'
-
-// configs/networks.ts
-export const networks = {
-  ethereum: { rpc: "...", contract: "...", quests: [...] },
-  polygon: { rpc: "...", contract: "...", quests: [...] },
-  arbitrum: { rpc: "...", contract: "...", quests: [...] },
-};
-
