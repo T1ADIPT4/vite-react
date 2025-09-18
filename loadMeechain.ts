97ea6ec7c1599d50cee481178e1e26fa1e10eea3
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-18 17:21:13 +0700

    Update loadMeechain.ts
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- loadMeechain.ts
+++ loadMeechain.ts
@@ -1,3 +1,7 @@
 import config from './meechain.json'
 
+const urlParams = new URLSearchParams(window.location.search);
+const networkKey = urlParams.get("network") || "ethereum";
+const config = networks[networkKey];
+
 export const getMeechainConfig = () => config
