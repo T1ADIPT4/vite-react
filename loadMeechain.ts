b546f0538008c73d7b6f9ed98329a989a0879b10
Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>
2025-09-18 17:29:26 +0700

    Update loadMeechain.ts
    Signed-off-by: Tanawat Prawanta <70209315+T1ADIPT4@users.noreply.github.com>

--- loadMeechain.ts
+++ loadMeechain.ts
@@ -4,4 +4,18 @@ const urlParams = new URLSearchParams(window.location.search);
 const networkKey = urlParams.get("network") || "ethereum";
 const config = networks[networkKey];
 
+function loadQuests(networkConfig) {
+  return networkConfig.quests.map((questId) => fetchQuestDefinition(questId));
+  query param (?network=polygon
+}
+
+const progressKey = `${userId}:${networkKey}`;
+saveProgress(progressKey, { questId: "mint-nft", status: "completed" });
+
+<select onChange={(e) => switchNetwork(e.target.value)}>
+  <option value="ethereum">Ethereum</option>
+  <option value="polygon">Polygon</option>
+  <option value="arbitrum">Arbitrum</option>
+</select>
+
 export const getMeechainConfig = () => config
